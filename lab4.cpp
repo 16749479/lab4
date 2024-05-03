876790d5c2f5212275227cb18c4ac0908220b789
16749479 <aleksiprot@gmail.com>
2024-05-03 18:01:06 +0300

    create structure

--- lab4.cpp
+++ lab4.cpp
@@ -1,1 +1,30 @@
-﻿
+﻿#include <iostream>
+#include <Windows.h>
+#include <Conio.h>
+#include <string>
+
+using namespace std;
+
+struct Unemployed
+{
+	string surname;
+	int age;
+	char gender;
+	string education;
+	string profession;
+	string position;
+	int last_position_experience;
+	int total_experience;
+	string registration_date;
+	double desired_salary;
+	string desired_position;
+
+}
+
+struct Node
+{
+	Unemployed data;
+	Node* next;
+};
+
+typedef Node* PNode;
