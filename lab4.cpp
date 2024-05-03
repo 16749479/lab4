f19710be217d12125a3fa8b3de678e65e7a520c7
16749479 <aleksiprot@gmail.com>
2024-05-03 18:55:00 +0300

    new function

--- lab4.cpp
+++ lab4.cpp
@@ -66,4 +66,46 @@ int main()
 	h = t = a = b = NULL;
 }
 
+void del(PNode& h, PNode& t)
+{
+	PNode p;
+	if (t == h)
+	{
+		delete(h);
+		h = t = NULL;
+		cout << "Очередь пуста " << endl;
+		return;
+	}
+	p = h->next;
+	delete (h);
+	h = p;
+}
 
+void see(PNode h)
+{
+	cout << "Вывод содержимого" << endl;
+	PNode h1;
+	h1 = h;
+	if (!h1)
+	{
+		cout << "Очередь пуста" << endl;
+		return;
+	}
+	do
+	{
+		cout << h1->data.surname << endl;
+		cout << h1->data.age << endl;
+		cout << h1->data.gender << endl;
+		cout << h1->data.education << endl;
+		cout << h1->data.profession << endl;
+		cout << h1->data.position << endl;
+		cout << h1->data.last_position_experience << endl;
+		cout << h1->data.total_experience << endl;
+		cout << h1->data.registration_date << std::endl;
+		cout << h1->data.desired_salary << endl;
+		cout << h1->data.desired_position << endl;
+		cout << endl;
+
+		h1 = h1->next;
+	} while (h1 != NULL);
+}
